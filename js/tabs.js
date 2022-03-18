29cb4f559acbe3d49198b975383904e51f23532d
Pavel Kuprianov <97345976+PavelKuprianov@users.noreply.github.com>
2022-03-18 23:20:31 +0300

    18.03.22

--- js/tabs.js
+++ js/tabs.js
@@ -2,14 +2,23 @@ const tabButtons = document.querySelectorAll('.design-list__item');
 const tabDescriptions = document.querySelectorAll('.design__descr');
 const tabImages = document.querySelectorAll('.design-images');
 const tabImagesLeft = document.querySelectorAll('.design-block__img');
+const title = document.querySelector('title');
+
+console.log(title)
 
 
 const changeContent = (array, value) => {
   array.forEach((elem) => {
-    if(elem.dataset.tabsField === value) {
-      elem.classList.remove('hidden')
-    }else {
+    if(elem.dataset.tabsField !== value) {
       elem.classList.add('hidden')
+    }else {
+      elem.classList.remove('hidden')
+
+      if (value === 'interior') {
+        title.textContent = "Примеры сайтов"
+      } else {
+        title.textContent = "Примеры приложений"
+      }
     }
   })
 } 
@@ -17,6 +26,8 @@ const changeContent = (array, value) => {
 tabButtons.forEach((tabButton) => {
   tabButton.addEventListener('click', (event) => {
     const dataValue = tabButton.dataset.tabsHandler
+
+    console.log(dataValue)
 
     changeContent(tabDescriptions, dataValue)
     changeContent(tabImages, dataValue)
